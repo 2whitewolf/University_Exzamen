729d016542c0a992edb316c67581b64cd6565a90
Bogdan Sevcenco <48834435+2whitewolf@users.noreply.github.com>
2025-06-20 18:57:32 +0300

    Initial Commit

---
+++ Codebase/CodebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodebaseApp.swift
+//  Codebase
+//
+//  Created by Bogdan on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
